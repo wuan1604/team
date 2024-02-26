d2dbd494de023fa35b209b52130f6aa57d8443cb
DucTri254 <131869747+DucTri254@users.noreply.github.com>
2024-02-26 17:03:42 +0700

    Add files via upload

---
+++ 123123.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(){
+	FILE *file;
+	file = fopen("text.txt","r");
+	char name[50];
+	//int age;
+	if(file == NULL){
+		printf("File khong ton tai");
+	}else{
+		printf("Mo file thanh cong\n");
+		/*fflush(file);
+		printf("\nNhap vao du lieu:");
+		gets(name);
+		printf("Nhap vao so tuoi:");
+		scanf("%d",&age);
+		fprintf(file,"Name = %s, Age = %d",name,age);*/
+	    while(!feof(file)){
+		    fscanf(file,"%s",&name);
+			printf("%s",name);
+		}
+		printf("\nNhap file thanh cong");
+		fclose(file);
+	}
+}
